ff435fda92523e8819586c1ce9b3bb5f21507d1e
Unknown <sanjuankenji@gmail.com>
2018-02-13 12:31:01 +0800

    Start "edit all task in one modal"
    Start "edit all task in one modal"

--- resources/views/PM_project_edit.blade.php
+++ resources/views/PM_project_edit.blade.php
@@ -1453,7 +1453,7 @@
 	
 	$('.editingrid').click(function () {
 	$('#alltasktable').html('');
-	var tableheader = '<thead style="background-color: #353959; color:#ffffff;"><tr><th width="25%">Task Description</th><th width="10%" style="text-align:center">Milestone</th><th width="10%">Complete %</th> <th>Status</th><th>Start Date</th><th>End Date</th></tr></thead>';
+	var tableheader = '<thead style="background-color: #353959; color:#ffffff;"><tr><th width="25%">Task Description</th><th width="10%" style="text-align:center">Milestone</th><th width="10%">Complete %</th><th width="10%">Start Date</th><th>End Date</th></tr></thead>';
 	$('#alltasktable').append(tableheader);  
 	
         $.ajax
@@ -1467,8 +1467,9 @@
 	                    var path 		= "/images/profile/" + data.emp_image;
 						var name 	= data.emp_first_name + ' ' + data.emp_middle_initial + ' ' + data.emp_last_name;
 						var col12 	= '<tr><td style="font-weight:normal">' + data.task_description + '</td><td style="font-weight:normal; text-align:center">' + data.phase_title+ '</td>';
-						var col3      = '<td><select class="form-control" id="select_taskpercent" name="select_taskpercent" style="font-weight:normal;"><option value="0">0%</option><option value="10">10%</option><option value="20">20%</option><option value="30">30%</option> <option value="40">40%</option><option value="50">50%</option> <option value="60">60%</option><option value="70">70%</option><option value="80">80%</option><option value="90">90%</option><option value="100">100%</option></select></td></tr>';
-							var row = col12 + col3;
+						var col3      = '<td><select class="form-control" id="select_taskpercent" name="select_taskpercent" style="font-weight:normal;"><option value="0">0%</option><option value="10">10%</option><option value="20">20%</option><option value="30">30%</option> <option value="40">40%</option><option value="50">50%</option> <option value="60">60%</option><option value="70">70%</option><option value="80">80%</option><option value="90">90%</option><option value="100">100%</option></select></td>';
+						var col4      = '<td><input type="date" class="form-control" name="task_stadate" id="task_stadate" value="' + data.pt_start_date + '" style="padding: 0px 0px 20px 0px; text-align:center; font-weight:normal"></td></tr>';
+						var row = col12 + col3 + col4;
 					$('#alltasktable').append(row);   
                 })
             }
